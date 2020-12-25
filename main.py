fixed policy gradient bug. Solved cartpole

main.py
@@ -1,3 +1,5 @@
+from random import seed
+
 import gym
 import torch
 import torch.nn as nn
@@ -37,7 +39,7 @@ def cartpoloe():
 
         opt_policy = learner.learn_policy(episodes=200)
 
-        #torch.save(q_model.module, "learned networks/cartpole/q_network.dnet")
+        #torch.save(q_model.module, "learned networks/cartpole/q_network.torch")
 
         agent = Agent(environment=environment, policy=opt_policy)
         input("add anything to continue")
@@ -45,7 +47,7 @@ def cartpoloe():
 
     else:
         environment = gym.make("CartPole-v1")
-        q_model = torch.load("learned networks/cartpole/q_network.dnet")
+        q_model = torch.load("learned networks/cartpole/q_network.torch")
         opt_policy = GreedyQPolicy(q_model)
         agent = Agent(environment=environment, policy=opt_policy)
         agent.perform_episode(render=True)
@@ -53,21 +55,28 @@ def cartpoloe():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    seed(2020)
+    torch.manual_seed(2020)
+
     def pl_grad():
         print("hi")
         environment = gym.make("CartPole-v1")
         a_model = nn.Sequential(
-            nn.Linear(4, 24),
+            nn.Linear(4, 40, bias=False),
             nn.ReLU(),
-            nn.Linear(24, 24),
-            nn.ReLU(),
-            nn.Linear(24, 2)
+            nn.Linear(40, 2, bias=False),
+            nn.Softmax(dim=1)
         )
 
-        optimizer = torch.optim.Adam(a_model.parameters(), lr=0.005)
+        optimizer = torch.optim.Adam(a_model.parameters(), lr=0.01)
+        learner = PolicyGradient(environment, a_model, optimizer, discount_factor=0.99)
+        opt_policy = learner.learn_policy(epochs=500, episodes_per_update=1)
 
-        learner = PolicyGradient(environment, a_model, optimizer)
-        learner.learn_policy(epochs=1000, episodes_per_update=20)
+        agent = Agent(environment=environment, policy=opt_policy)
+        input("add anything to continue")
+        agent.perform_episode(render=True)
 
-    cartpoloe()
+        torch.save(a_model, "learned networks/cartpole/a_network.torch")
+
+    pl_grad()
 

policy_gradient.py
@@ -10,8 +10,6 @@ from policy import Policy
 from pytorch_utilities import list_of_tuples_to_tuple_of_tensors, get_reward_to_go
 from type_definitions import StateType
 
-score = []
-
 
 class StochasticPolicy(Policy):
 
@@ -26,7 +24,7 @@ class StochasticPolicy(Policy):
         x = torch.tensor([state], dtype=torch.float32)
         distribution = self.a_model(x)
         # TODO Check if correct
-        m = Categorical(logits=distribution)
+        m = Categorical(distribution)
         action = m.sample().item()
         return action
 
@@ -42,23 +40,29 @@ class PolicyGradient(Learner):
         self.optimizer.zero_grad()
         state_index = 0
         action_index = 1
+        reward_history = []
 
         policy = StochasticPolicy(self.a_model)
 
         for _ in tqdm(range(epochs)):
             for _ in range(episodes_per_update):
                 # collect trajectory and calculate reward to go
-                trajectory = self.collect_trajectory(policy)
-                reward_to_go = get_reward_to_go(trajectory, self.discount_factor)
+                trajectory, trajectory_reward = self.collect_trajectory(policy)
+                reward_history.append(trajectory_reward)
+                advantage = get_reward_to_go(trajectory, self.discount_factor)
 
                 # convert to pytorch tensors
                 trajectory = list_of_tuples_to_tuple_of_tensors(trajectory)
-                reward_to_go = torch.tensor(reward_to_go, dtype=torch.float32)
+                advantage = torch.tensor(advantage, dtype=torch.float32)
 
                 # calculate loss
-                policy_loss = Categorical(logits=self.a_model(trajectory[state_index]))
-                policy_loss = policy_loss.log_prob(trajectory[action_index]) * reward_to_go
-                policy_loss = policy_loss.mean()
+                policy_loss = Categorical(self.a_model(trajectory[state_index]))
+                policy_loss = - policy_loss.log_prob(trajectory[action_index]) * advantage
+                policy_loss = torch.sum(policy_loss)
+
+                # to take the expected gradient of episodes_per_update episodes,
+                # we divide the loss by episodes_per_update
+                policy_loss = policy_loss/episodes_per_update
 
                 # accumulate gradient
                 policy_loss.backward()
@@ -67,7 +71,7 @@ class PolicyGradient(Learner):
             self.optimizer.step()
             self.optimizer.zero_grad()
 
-        plt.plot(score)
+        plt.plot(reward_history)
         plt.xlabel('episode')
         plt.ylabel('total reward')
         plt.savefig("score.png")
@@ -84,6 +88,4 @@ class PolicyGradient(Learner):
 
         agent.perform_episode()
 
-        score.append(r_obs.get_rewards()[-1])
-
-        return t_obs.get_trajectories()[0]
+        return t_obs.get_trajectories()[0], r_obs.get_rewards()[0]